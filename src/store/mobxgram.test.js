bfdf3859522a7894a543e28d925650c097ad0fa4
shoaibbhimani <shoaibbhimani1392@gmail.com>
2018-01-26 04:35:12 +0530

    Increment Likes and Adding Comments

--- src/store/mobxgram.test.js
+++ src/store/mobxgram.test.js
@@ -4,7 +4,11 @@ import { toJS } from "mobx";
 import store from "./sampleStore";
 
 describe("Testing Mobxgram State", () => {
-  let mobxgramInstance = new Mobxgram();
+  let mobxgramInstance = null;
+
+  beforeEach(function() {
+    mobxgramInstance = new Mobxgram();
+  });
 
   it("Default State Testing", () => {
     //input => output
@@ -14,5 +18,26 @@ describe("Testing Mobxgram State", () => {
   it("Getting Default data from sample Store", () => {
     mobxgramInstance.getStore();
     expect(toJS(mobxgramInstance.mobxgramList)).toEqual(store);
+  });
+
+  it("Increment Likes for Each Photo", () => {
+    mobxgramInstance.mobxgramList.push(store[0]);
+
+    mobxgramInstance.incrementLikes(0);
+
+    expect(mobxgramInstance.mobxgramList[0].likes).toBe(++store[0].likes);
+  });
+
+  it("Adding Comments to Photo", () => {
+    mobxgramInstance.mobxgramList.push(store[0]);
+    mobxgramInstance.addComments({
+      index: 0,
+      author: "Shoaib",
+      text: "Testing Comments"
+    });
+
+    expect(mobxgramInstance.mobxgramList[0].comments.length).toBe(
+      ++store[0].comments.length
+    );
   });
 });
